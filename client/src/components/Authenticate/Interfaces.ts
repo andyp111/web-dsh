5f456f9ecc2a2349cf09a725614797b8f26814d3
Andy Pham <andy.pham111@gmail.com>
2021-06-07 12:44:12 -0700

    signup comp created

---
+++ client/src/components/Authenticate/Interfaces.ts
@@ -0,0 +1,11 @@
+export interface ILogIn {
+    username: string;
+    password: string;
+}
+
+export interface ISignUp {
+    username: string;
+    password: string;
+    email: string;
+    level: string;
+}
---
+++ client/src/components/Authenticate/LogIn.tsx
@@ -0,0 +1,45 @@
+import * as React from 'react';
+import { useState, ChangeEvent, MouseEvent } from 'react';
+import axios from 'axios';
+import { ILogIn } from './Interfaces';
+
+export const LogIn = () => {
+    const [username, setUsername] = useState<string>('');
+    const [password, setPassword] = useState<string>('');
+    const [loginInfo, setLoginInfo] = useState<ILogIn>({username, password})
+
+    const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
+        if (event.target.name === "username") setUsername(event.target.value);
+        if (event.target.name === "password") setPassword(event.target.value);
+    }
+
+    const loginClick = (event: MouseEvent): void => {
+        event.preventDefault();
+        setLoginInfo({username: username, password: password});
+    }
+
+    return (
+        <div className="login-container">
+            <div className="login-forms">
+                <input
+                    type="text" 
+                    placeholder="username..."
+                    name="username"
+                    value={username}
+                    onChange={handleChange}
+                />
+                <input
+                    type="password"
+                    placeholder="password..."
+                    name="password"
+                    value={password}
+                    onChange={handleChange} 
+                />
+            </div>
+            <div className="login-button">
+                <button />
+            </div>
+
+        </div>
+    )
+}
