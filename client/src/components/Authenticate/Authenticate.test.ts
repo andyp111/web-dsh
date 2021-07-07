e8f5b2774a729af602491e8b5b1397d6ae412450
Andy Pham <andy.pham111@gmail.com>
2021-07-07 12:22:53 -0700

    added more tests for authenticate

--- client/src/components/Authenticate/Authenticate.test.ts
+++ client/src/components/Authenticate/Authenticate.test.ts
@@ -1,61 +1,77 @@
-const app = require('../../../../server/app.js');
-const request = require('supertest');
+const app = require("../../../../server/app.js");
+const request = require("supertest");
 
-describe("Check if test work at all", () => {
-  it("should return 2 for adding 1 + 1", () => {
-    expect(1 + 1).toBe(2);
-  })
-})
+const login = {
+  username: "jordan",
+  password: "hoang",
+  email: "jordan@hoang",
+  level: "junior",
+};
+
+const signup = {
+  username: "ben",
+  password: "pham",
+  email: "ben@pham",
+  level: "Junior",
+};
 
 describe("Should fail with already existing login information", () => {
   it("should return a 400 code for already existing login information", async () => {
-    const response = await request(app)
-      .post('/api/signup')
-      .send({
-        username: 'jordan',
-        password: 'hoang',
-        email: 'jordan@hoang',
-        level: 'Junior'
-      });
+    const response = await request(app).post("/api/signup").send({
+      username: "jordan",
+      password: "hoang",
+      email: "jordan@hoang",
+      level: "Junior",
+    });
 
     expect(response.status).toBe(400);
     console.log(response.status);
     console.log(response.body.message);
-  })
-})
+  });
+});
 
 describe("Should create a new user and delete it successfully", () => {
   it("should return a 200 successful code for deleted user", async () => {
-    const newUser = await request(app)
-      .post('/api/signup')
-      .send({
-        username: 'andy',
-        password: 'hoang',
-        email: 'andy@hoang',
-        level: 'Junior'
-      });
+    const newUser = await request(app).post("/api/signup").send({
+      username: "andy",
+      password: "hoang",
+      email: "andy@hoang",
+      level: "Junior",
+    });
 
     expect(newUser.status).toBe(200);
     console.log(newUser.status);
-    expect(newUser.body.message).toBe('inserted')
-    console.log(newUser.body.message);
-  })
-})
-
+  });
+});
+//will fail when signing up more than once
 describe("Should create a new user and post it into the database", () => {
   it("should return a 200 successful code for newly created user", async () => {
-    const response = await request(app)
-      .post('/api/login')
-      .send({
-        username: 'ben',
-        password: 'pham',
-        email: 'ben@pham',
-        level: 'Junior'
-      });
+    const response = await request(app).post("/api/signup").send(signup);
 
     expect(response.status).toBe(200);
     console.log(response.status);
-    expect(response.body.message).toBe('inserted')
-    console.log(response.body.message);
+  });
+});
+
+
+describe("Should fail to login user with incorrect password", () => {
+  it("should return 400 for incorrect login password", async () => {
+    const response = await request(app).post("/api/login").send({
+      username: 'jordan',
+      password: 'incorrectpassword'
+    })
+
+    expect(response.status).toBe(400);
+  })
+})
+
+describe("Should correctly login user with correct username and password", () => {
+  it("should return 200 for correct login password and username", async () => {
+    const response = await request(app).post("/api/login").send({
+      username: 'jordan',
+      password: 'hoang'
+    })
+
+    expect(response.status).toBe(200);
   })
 })
